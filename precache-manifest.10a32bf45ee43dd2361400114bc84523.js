3542c0507e80535e7446bbfe664602fd5b4624ae
Silvaire <silvain.toromanoff@gmail.com>
2019-09-17 00:28:24 +0300

    Updates

---
+++ precache-manifest.10a32bf45ee43dd2361400114bc84523.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3babcbde06acf133cdd078474732c134",
+    "url": "/finskblivningsfest/index.html"
+  },
+  {
+    "revision": "d13376d306e577aa01ce",
+    "url": "/finskblivningsfest/static/js/2.e4dd18c0.chunk.js"
+  },
+  {
+    "revision": "fa8da596653988e4674a",
+    "url": "/finskblivningsfest/static/js/main.5ee22df5.chunk.js"
+  },
+  {
+    "revision": "55dd1d3bddb2ea990fa1",
+    "url": "/finskblivningsfest/static/js/runtime~main.9c5f6912.js"
+  },
+  {
+    "revision": "fd0be20226ba8938f9a5457d1720fa09",
+    "url": "/finskblivningsfest/static/media/logo.fd0be202.jpg"
+  }
+]);
--- src/index.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import AppWrapper from './AppWrapper';
-
-ReactDOM.render(<AppWrapper />, document.getElementById('root'));
